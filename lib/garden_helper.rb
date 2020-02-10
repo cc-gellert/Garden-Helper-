421b28ada03f15ad15f67302c79ec869482b8abf
Chloe Gell <gellert.chloe@gmail.com>
2020-02-10 16:49:19 +0000

    Initial commit

---
+++ lib/garden_helper.rb
@@ -0,0 +1,6 @@
+require "garden_helper/version"
+
+module GardenHelper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/garden_helper_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GardenHelper do
+  it "has a version number" do
+    expect(GardenHelper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
