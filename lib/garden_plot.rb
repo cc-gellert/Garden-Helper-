a0f3ed32ed601b0138b5f005f465a51ae95ee08f
Chloe Gell <gellert.chloe@gmail.com>
2020-02-10 21:41:56 +0000

    added error handling to garden plot add/remove methods

--- lib/garden_plot.rb
+++ lib/garden_plot.rb
@@ -15,21 +15,29 @@ class GardenPlot
   def save 
     @@all << self 
   end 
-  def removePlant(plant_name, number=1)
-    @plants[plant] = number 
+  def removePlant(plant, number=1)
     found_plant_object = Plant.find_by_name(plant)
-    @footageleft -= (number * found_plant_object[spacing]) 
+    if(@plants[plant] >= number )
+      @plants[plant] -= number 
+      @footageleft -= (number * found_plant_object[spacing])
+    else 
+      puts "There are only #{@plants[plant]} #{plant} plants here."
   end 
   def addPlant(plant, number=1)
-    @plants[plant] += number || @plants[plant] = number 
     found_plant_object = Plant.find_by_name(plant)
-    @footageleft -= (number * found_plant_object[spacing]) 
+    needed_space = found_plant_object[spacing] * number 
+    if(@footageleft >= needed_space)
+      @plants[plant] += number || @plants[plant] = number 
+      @footageleft -= needed_space  
+    else 
+      puts "There is not enough space in this plot."
+    end 
   end 
   def self.find_by_name(name)
     self.all.select {|plot| plot.name == name}
   end
   def self.delete_by_name(name)
-    self.all.sort {|plot| plot.name != name} 
+    self.all.filter {|plot| plot.name != name} 
   end 
   def print
     puts "#{self.name} garden plot is #{self.footage} square feet and has:"
