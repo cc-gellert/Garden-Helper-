344b20306a73876e25af311b888b1370a42fd3c8
Chloe Gell <gellert.chloe@gmail.com>
2020-02-11 16:32:57 +0000

    refactored cli methods

--- lib/cli.rb
+++ lib/cli.rb
@@ -29,60 +29,34 @@ class CommandLineInterface
     while input != "exit"
       case input 
       when "lookup a plant"
-        puts "Great! What plant are you looking for?"
-        name = gets.strip.capitalize 
-        display_plant(name)
+        display_plant
         ask_for_input 
       when "lookup a garden plot"
-        puts "Ok, what is the name of the plot you are looking for?"
-        name = gets.strip 
-        display_plot(name)
+        display_plot
         ask_for_input
-      when "list all plants"
+      when "list all plants" || "list plants"
         display_plants 
         ask_for_input
-      when "view garden plots"
+      when "view garden plots" || "view plots"
         display_plots 
         ask_for_input
-      when "plan a new garden plot"
-        puts "Great! What would you like to call this new plot?"
-        name = gets.strip 
-        puts "Ok, got it. And how many square feet does it have?"
-        footage = gets.strip 
-        new_plot = GardenPlot.new(footage, name)
-        new_plot.print 
+      when "plan a new garden plot" || "new plot"
+        add_plot 
         ask_for_input
-      when "edit a garden plot"
-        puts "Ok, great. What is the name of the plot you would like to change?"
-        name = gets.strip 
-        display_plot(name)
-        puts "Would you like to add plants, remove plants, or delete this plot?"
-        input = gets.strip 
-        if input == "add plants"
-          puts "What plant would you like to add?"
-          plant = gets.strip.capitalize 
-          puts "Ok, how many would you like to add?"
-          number = gets.strip 
-          found_plot.addPlant(plant, number)
-        elsif input == "remove plants"
-          puts "What plant would you like to remove?"
-          plant = gets.strip.capitalize 
-          puts "Ok, how many would you like to add?"
-          number = gets.strip 
-          found_plot.removePlant(plant, number)
-        elsif input == "delete this plot"
-          GardenPlot.delete_by_name(found_plot.name) 
-        else 
-          puts "I'm sorry, that's not a recognized command."
-        end 
+      when "edit a garden plot" || "edit plot"
+        edit_plot 
         ask_for_input
       else 
         puts "Sorry, that doesn't look like a recognized command. You may lookup a plant, lookup a garden plot, list all plants, view garden plots, plan a new garden plot, edit a garden plot, or exit."
-        ask_for_input
-      end
+      end 
     end 
   end 
-  def display_plant(plant)
+  
+  #other methods 
+  
+  def display_plant
+    puts "Great! What plant are you looking for?"
+    name = gets.strip.capitalize 
     found_plant = Plant.find_by_name(name)
     if(found_plant)
       found_plant.print
@@ -104,13 +78,48 @@ class CommandLineInterface
       end 
     end 
   end 
-  def display_plot(name)
+  def display_plot
+    puts "Ok, what is the name of the plot you are looking for?"
+    name = gets.strip 
     found_plot = GardenPlot.find_by_name(name)
     if(found_plot)
       found_plot.print 
     else 
       puts "Sorry, there isn't a plot by that name in this collection."
     end 
+  end
+  def add_plot 
+    puts "Great! What would you like to call this new plot?"
+    name = gets.strip 
+    puts "Ok, got it. And how many square feet does it have?"
+    footage = gets.strip 
+    new_plot = GardenPlot.new(footage, name)
+    new_plot.print 
   end 
-end
+  def edit_plot
+      puts "Ok, great. What is the name of the plot you would like to change?"
+      name = gets.strip 
+      display_plot(name)
+      puts "Would you like to add plants, remove plants, or delete this plot?"
+      input = gets.strip.downcase  
+      while input != "quit"
+      if input == "add plants" || "add"
+        puts "What plant would you like to add?"
+        plant = gets.strip.capitalize 
+        puts "Ok, how many would you like to add?"
+        number = gets.strip 
+        found_plot.addPlant(plant, number)
+      elsif input == "remove plants" || "remove"
+        puts "What plant would you like to remove?"
+        plant = gets.strip.capitalize 
+        puts "Ok, how many would you like to add?"
+        number = gets.strip 
+        found_plot.removePlant(plant, number)
+      elsif input == "delete this plot" || "delete"
+        GardenPlot.delete_by_name(name) 
+      else 
+        puts "I'm sorry, that's not a recognized command."
+      end 
+    end 
+  end
 end 
