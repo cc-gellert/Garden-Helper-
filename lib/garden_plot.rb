830badbc3af73d228aa21ff42dbc8ccdf23587c3
Chloe Gell <gellert.chloe@gmail.com>
2020-02-11 16:40:37 +0000

    corrected print method in GardenPlot class

--- lib/garden_plot.rb
+++ lib/garden_plot.rb
@@ -19,13 +19,13 @@ class GardenPlot
     found_plant_object = Plant.find_by_name(plant)
     if(@plants[plant] >= number )
       @plants[plant] -= number 
-      @footageleft -= (number * found_plant_object[spacing])
+      @footageleft += (number * found_plant_object.spacing)
     else 
       puts "There are only #{@plants[plant]} #{plant} plants here."
   end 
   def addPlant(plant, number=1)
     found_plant_object = Plant.find_by_name(plant)
-    needed_space = found_plant_object[spacing] * number 
+    needed_space = found_plant_object.spacing * number 
     if(@footageleft >= needed_space)
       @plants[plant] += number || @plants[plant] = number 
       @footageleft -= needed_space  
@@ -40,10 +40,10 @@ class GardenPlot
     self.all.select {|plot| plot.name != name} 
   end 
   def print
-    puts "#{self.name} garden plot is #{self.footage} square feet and has:"
-    self.plants.each do |plant, value|
+    puts "#{@name} garden plot is #{@footage} square feet and has:"
+    @plants.each do |plant, value|
       puts "#{value} #{plant} plants."
     end 
-    puts "There is #{self.footageleft} square feet left in this plot."
+    puts "There is #{@footageleft} square feet left in this plot."
   end 
 end 
