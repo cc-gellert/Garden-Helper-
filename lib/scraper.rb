a1c6a6b5c0092e609f4922ab6fc994a93ea51b1c
Chloe Gell <gellert.chloe@gmail.com>
2020-02-11 17:14:04 +0000

    added scraping to Scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,8 @@ class Scraper
   def self.scrape_index_page(url)
     index = Nokogiri::HTML(open(url))
     plants = [] 
-    index.css("").each do |plant| 
-      plant_name = "#{student.attr('href')}"
+    index.css("ion-no-padding.item.md.in-list.ion-focusable.hydrated").each do |plant| 
+      plant_name = plant.css("h2.ion-text-wrap").text
       plant_profile_url = blablabla 
       plants << {name: plant_name, profile: plant_profile_url}
     end 
